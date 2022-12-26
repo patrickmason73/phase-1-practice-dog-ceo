dbd3cf68c919490e57afbdc9752db9923e16e999
Patrick Mason <patrickmason73@gmail.com>
2022-12-26 14:19:12 -0600

    commit tree 01bba132ae77b7d02b58d9b1e75c12f2317e0925

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', 
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(res => res.json())
+.then(data => {
+   console.log(data)
+   for (const img of data) {
+    document.append(img)
+   }
+
+   })
+    
+)
+
+
+// function appendImg(img) {
+//     img.forEach(element => {
+//         document.append(element)
+//     });
+// }
